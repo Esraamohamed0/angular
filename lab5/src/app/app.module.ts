e143e3addcb03b5946c6f5433b31bbcbb68f67e3
Esraamohamed0 <esraaelsyed320@gmail.com>
2023-05-01 19:04:05 +0200

    lab5

---
+++ lab5/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { ErrorComponent } from './components/error/error.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { UsersDetailsComponent } from './components/users-details/users-details.component';
+import { UsersComponent } from './components/users/users.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddUserComponent,
+    ErrorComponent,
+    HeaderComponent,
+    HomeComponent,
+    UpdateUserComponent,
+    UsersDetailsComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
